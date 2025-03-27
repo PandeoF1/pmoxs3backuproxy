39e3e62ae686ad4634dc152b7a42135e890d09e2
Jonathan Milhas <jonathan.milhas@it-development.com>
2025-03-27 17:39:58 +0100

    feat: "Remove unused GetCollector function"

--- cmd/pmoxs3backuproxy/main.go
+++ cmd/pmoxs3backuproxy/main.go
@@ -202,7 +202,6 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	cookiere := regexp.MustCompile(`PBSAuthCookie=([^;]+)`)
 	matches := cookiere.FindStringSubmatch(r.Header.Get("Cookie"))
 	auth := false
-	collector := s3pmoxcommon.GetCollector()
 
 	C := TicketEntry{}
 	if len(matches) >= 2 {
@@ -432,7 +431,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 					 * a size field, otherwise it assumes the backup to be active
 					 * and ignores it during sync
 					 **/
-					Size:  uint64(collector.GetSnapshotSize(snap.S3Prefix())),
+					Size:  200,
 					Owner: C.AccessKeyID + "@pbs",
 				}
 				var exists bool = false
@@ -1219,8 +1218,6 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte(err.Error()))
 				return
 			}
-			collector = s3pmoxcommon.NewSizeCollection(time.Duration(30)*time.Second, "/backups")
-			collector.SetClient(minioClient)
 			connectionList[username] = minioClient
 
 			_, listerr := connectionList[username].ListBuckets(context.Background())
@@ -1235,7 +1232,6 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 		te.Client = connectionList[username]
 		s.Auth.Store(ticket.Ticket, te)
-		collector.SetClient(te.Client)
 
 		respbody, _ := json.Marshal(resp)
 		s3backuplog.DebugPrint(string(respbody))
